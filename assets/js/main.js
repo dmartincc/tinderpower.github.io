336e82ee6d633731bafede0db08d0bdd97510659
fermarinsanchez <fermarinsanchez@yahoo.es>
2019-12-06 22:17:02 +0100

    pasaba por aquí...

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+};
+
+console.log(getUrlVars()['modal']);
+
+if (getUrlVars()['modal'] === true) {
+  $(window).on('load',function(){
+    $('#exampleModalCenter').modal('show');
+    });
+}
