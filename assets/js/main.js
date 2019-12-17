fea0d8d0099beab6c044fdae97412ee6fe8632da
Fernando <fernando@MacBookFernando.home>
2019-12-17 08:51:04 +0100

    JS smoothscroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,30 @@
-function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = value;
-    });
-    return vars;
-};
+(function() {
+  scrollTo();
+})();
 
-console.log(getUrlVars()['modal']);
+function scrollTo() {
+  const links = document.querySelectorAll(".scroll");
+  links.forEach(each => (each.onclick = scrollAnchors));
+}
 
-if (getUrlVars()['modal'] === true) {
-  $(window).on('load',function(){
-    $('#exampleModalCenter').modal('show');
-    });
+function scrollAnchors(e, respond = null) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+  e.preventDefault();
+  var targetID = respond
+    ? respond.getAttribute("href")
+    : this.getAttribute("href");
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+  const checkIfDone = setInterval(function() {
+    const atBottom =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = "-1";
+      targetAnchor.focus();
+      window.history.pushState("", "", targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
 }
