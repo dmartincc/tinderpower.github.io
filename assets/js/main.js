b84a4e5a98fcdce6ca012822ef84600681858a2d
Fernando <fernando@MacBook-de-Fernando.local>
2019-12-18 13:13:03 +0100

    include política de privacidad in form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,21 @@
-/*(function() {
+(function() {
   scrollTo();
 })();
 
 function scrollTo() {
-  const links = document.querySelectorAll(".scroll");
-  links.forEach(each => (each.onclick = scrollAnchors));
+  var links = document.getElementsByTagName("a");
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    if (
+      link.href &&
+      link.href.indexOf("#") != -1 &&
+      (link.pathname == location.pathname ||
+        "/" + link.pathname == location.pathname) &&
+      link.search == location.search
+    ) {
+      link.onclick = scrollAnchors;
+    }
+  }
 }
 
 function scrollAnchors(e, respond = null) {
@@ -28,4 +39,4 @@ function scrollAnchors(e, respond = null) {
     }
   }, 100);
 }
-console.log("probando");*/
+console.log("probando");
